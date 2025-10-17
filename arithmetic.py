6c3a71ce787a65f2bf03a8ea4ad1d82921d1c254
Fritz Cartagena <fritzcartagena07@gmail.com>
2025-10-17 16:08:22 +0800

    Feat: Update arithemtic

--- arithmetic.py
+++ arithmetic.py
@@ -1,4 +1,41 @@
-x = input()
+num1 = input()
+operation = input()
+num2 = input()
 
-for i in range(int(x)):
-    print(f"{i} = Hello World")
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def division(a, b):
+    if b == 0:
+        return "Error: Division by zero"
+    else:
+        return a / b
+    
+def multiply(a, b):
+    return a * b
+
+def squareRoot(a):
+    if a < 0:
+        return "Error: Square root of negative number"
+    else:
+        return a ** 0.5
+    
+def power(a, b):
+    return a ** b
+
+match operation:
+    case "+":
+        print(add(num1, num2))
+    case "-":
+        print(subtract(num1, num2))    
+    case "/":
+        print(division(num1, num2))
+    case "*":
+        print(multiply(num1, num2))
+    case "sqrt":
+        print(squareRoot(num1))
+    case "^":
+        print(power(num1, num2))
